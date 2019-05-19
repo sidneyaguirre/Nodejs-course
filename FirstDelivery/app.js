57928c4b859176c532c428008c6092dd5f3c32ac
sidneyaguirre <sidney.aguirrec@gmail.com>
2019-05-19 16:37:22 -0500

    enrollments

--- FirstDelivery/app.js
+++ FirstDelivery/app.js
@@ -1,17 +1,85 @@
-const { courses, find_subject } = require('./courses');
-let { id, name, duration, cost } = courses;
+const { courses } = require('./courses');
 
-function find_subject(a){
-   let subject = courses.find(subject => subject.id === a);
-   console.log(a);
-   return subject;
+const fs = require('fs')
+
+/* Declares the details of the enrollment: 
+course id, name of the interested person & 
+citizen card id number */
+signup = {
+    name: {
+        alias: 'n',
+        demand: true,
+    },
+    idnumber: {
+        alias: 'i',
+        demand: true,
+    },
+    courseid: {
+        alias: 'c',
+        demand: true,
+    }
 }
 
+/* allows the implementation of the command line for the enrollment */
+const argv = require('yargs')
+    .command('inscribir', 'enroll a course', signup)
+    .argv
+
+/* Print all the courses offered and their information */
+//function definition: 
 let showCourses = () => {
-   for(let i = 0; i < courses.length; i++) {
-      setTimeout(function(){
-         console.log(courses[i])
-      }, i*2000);
-   };
+    for(let i = 0; i < courses.length; i++) {
+        setTimeout(function() {
+            console.log(courses[i])
+        }, i * 2000);
+    };
+}; //function calling:
+//showCourses();
+
+
+/* To find a course given its id */
+//function definition: 
+function find_subject(a) {
+    let subject = courses.find(subject => subject.id === a);
+    if(subject == undefined) {
+        console.log('No existe el curso con el id ' + a + '. Por favor verifique la informacion')
+    }
+    return subject;
 };
-showCourses();
+
+
+/* Creation of the file within the details of the enrollment */
+//function definition: 
+let signup_file = (course, name, identification) => {
+    /* gets the id provided by the person who wants to enroll the course */
+    let ci = course;
+    let ip = name;
+    let idn = identification;
+    let subject = find_subject(ci);
+    if(subject == undefined) {
+        console.log('No es posible realizar la inscripcion')
+        return;
+    };
+    /* Creates the text for the file */
+    let enrollmentDetails = 'Nombre del estudiante: ' + ip + '\n' +
+        'Cédula del estudiante: ' + idn + '\n' + 'Id del curso: ' + subject.id + '\n' +
+        'materia: ' + subject.name + '\n' +
+        'duracion: ' + subject.duration + '\n' +
+        'costo: ' + subject.cost + '\n';
+    let filename = 'prematricula' + ip + idn;
+    let filepath = './enrollments/' + filename;
+    fs.writeFile(filepath, enrollmentDetails, (err) => {
+        if(err) throw err;
+        console.log('Inscripción realizada.');
+    });
+};
+
+/* gets the id of the course provided by the person who wants to enroll the course */
+let ci = argv.courseid;
+/* gets the name of the person who wants to enroll the course */
+let ip = argv.name;
+/* gets the identification number of the person who wants to enroll the course */
+let idn = argv.idnumber;
+
+/* Creates the enrollment file with the course information and interested person */
+signup_file(ci, ip, idn);
--- FirstDelivery/courses.js
+++ FirstDelivery/courses.js
@@ -1,37 +1,51 @@
+/* List of courses available */
+
 let courses = [
    {
       id: 10,
-      name: 'Linear Algebra',
+      name: 'Algebra Lineal',
       duration: 3, //month
       cost: 2500, //COP
    }, 
    {
-      id: 12,
-      name: 'Programming',
+      id: 11,
+      name: 'Programacion',
       duration: 2, //month
       cost: 1500, //COP
    },
    {
-      id: 13,
-      name: 'English',
+      id: 12,
+      name: 'Ingles',
       duration: 3, //month
       cost: 2000, //COP
    },
    {
-      id: 14,
-      name: 'History',
+      id: 13,
+      name: 'Historia',
       duration: 2, //month
       cost: 800, //COP
    },
    {
-      id: 16,
-      name: 'Geometry',
+      id: 14,
+      name: 'Fisica',
       duration: 3, //month
+      cost: 2000, //COP
+   },
+   {
+      id: 15,
+      name: 'Lectoescritura',
+      duration: 2, //month
+      cost: 2000, //COP
+   },
+   {
+      id: 16,
+      name: 'Filosofia',
+      duration: 2, //month
       cost: 2000, //COP
    },
 ];
 
-
+/* Exports the list of courses */
 module.exports = {
    courses,
 };
